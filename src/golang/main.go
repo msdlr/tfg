4714e3f6f8618ada26ef15ff45afaf7112f73093
msdlr <miguelsrosa97@gmail.com>
2020-11-16 12:12:17 +0100

    client connected

--- src/golang/main.go
+++ src/golang/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	// Dial address: ganache in localhost
+	conn, err := ethclient.Dial("http://localhost:8545")
+	if err != nil {
+		log.Fatal("Error reaching RCP.", err)
+	}
+
+	ctx := context.Backgraoung()
+	tx, pending, _ := conn.TranstactionByHash(ctx, common.HexToHash(""))
+
+	if !pending {
+		fmt.Println(tx)
+	}
 }
