df9d93fd4c3296d1e94e1602dfc05e927239b530
msdlr <miguelsrosa97@gmail.com>
2020-11-16 13:36:47 +0100

    dial() function

--- src/golang/main.go
+++ src/golang/main.go
@@ -3,22 +3,39 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
-	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func main() {
+func dial(url string) {
 	// Dial address: ganache in localhost
-	conn, err := ethclient.Dial("http://localhost:8545")
+	conn, err := ethclient.Dial(url)
 	if err != nil {
 		log.Fatal("Error reaching RCP.", err)
+		fmt.Println("Connection to provided URL failed")
 	}
 
-	ctx := context.Backgraoung()
-	tx, pending, _ := conn.TranstactionByHash(ctx, common.HexToHash(""))
 
-	if !pending {
-		fmt.Println(tx)
+	fmt.Println("Connection to "+ url+" successful!")
+	_ = conn // we'll use this in the upcoming sections
+}
+
+func main(){
+	/*
+	Param checking:
+	If none: default (localhost)
+	1: url of node to stablich connection to.
+	 */
+
+	// No parameters provided
+	var url string
+	if len(os.Args) == 1 {
+		url = "http://localhost:8545"
+	} else {
+		url = os.Args[1]
 	}
+
+	// Stablish connection to the blockchain
+	dial(url)
 }
