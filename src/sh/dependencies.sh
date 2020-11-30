7cc63fa3871dcb83858b97add231ba830a48eabf
msdlr <miguelsrosa97@gmail.com>
2020-11-30 11:35:34 +0100

    fixed ethereum install script not adding both ppas at the same time

---
+++ src/sh/dependencies.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This scripts installs the dependencies (go and packages used) for the project
+
+sudo apt install golang-go -y # The one in ubuntu/debian? repos
+
+# Golang packages:
+github.com/ethereum/go-ethereum # Ethereum
