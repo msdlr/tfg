f4dc4cdb01b546411503328101b29d79014148b1
msdlr <miguelsrosa97@gmail.com>
2020-12-07 09:27:18 +0100

    added golang packages in dependencies script

--- src/sh/dependencies.sh
+++ src/sh/dependencies.sh
@@ -5,4 +5,6 @@
 sudo apt install golang-go -y # The one in ubuntu/debian? repos
 
 # Golang packages:
-github.com/ethereum/go-ethereum # Ethereum
+go get github.com/ethereum/go-ethereum # Ethereum
+go get github.com/gorilla/websocket
+go get github.com/shirou/gopsutil/cpu
