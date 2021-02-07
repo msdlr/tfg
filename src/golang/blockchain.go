3d687bb75ab1668f9faf9849a3015f2d5a7ab023
msdlr <miguelsrosa97@gmail.com>
2021-02-07 21:23:07 +0100

    testing and renewal of blockchain methods

--- src/golang/blockchain.go
+++ src/golang/blockchain.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
 	"os"
+
+	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/ethclient"
 )
 
-func contactBlockchain(url string) error {
+func ContactBlockchain(url string) error {
 	// Dial address: ganache in localhost
 	conn, err := ethclient.Dial(url)
 	if err != nil {
@@ -20,45 +22,42 @@ func contactBlockchain(url string) error {
 	return err
 }
 
-// Load an eth keypair with a password, from the keystore path
-func openAccount(_pubKey string, _password string) {
+// openAccount loads an eth keypair with a password, from the keystore path
+func OpenAccount(ks *keystore.KeyStore, _pubKey string, _password string) uint8 {
 
 	// This returns an array with the keys stored in the keystore path
 	var ethAccArray = ks.Accounts()
-	var success bool = false
 
 	// Iterate through every account to find which pub key coincides
-	for i:= 0; i< len(ethAccArray); i++ {
+	for i := 0; i < len(ethAccArray); i++ {
 		// Check if the pub key is the same as the one provided
-		if _pubKey == ethAccArray[i].Address.Hex()  {
+		if _pubKey == ethAccArray[i].Address.Hex() {
 			err := ks.Unlock(ethAccArray[i], _password)
 			if err == nil {
-				success = true
-				os.Setenv("ETHACC",ethAccArray[i].Address.Hex())
+				os.Setenv("ETHACC", ethAccArray[i].Address.Hex())
 				// Account is unlocked, we can get out of the iterating loop
 				fmt.Println("Account " + _pubKey + " unlocked!")
-				break
+				return 1
 			} else {
 				// There has been an error unlocking the account
-				success = false
 				fmt.Println("Account was found but not unlocked")
+				return 2
 			}
 		}
 	}
 	// If no account was unlocked
-	if !success {
-		fmt.Println("Account was not found")
-	}
+	return 3
 }
 
-// New keypair
-func newAccount(_pass string) string {
+// CreatenewAccount: New keypair
+func CreatenewAccount(ks *keystore.KeyStore, _pass string) string {
 	// Encrypt private key
 	account, err := ks.NewAccount(_pass)
 
 	// Error checking
 	if err != nil {
 		log.Fatal(err)
+		return ""
 	}
 	// fmt.Println(account.Address.Hex())
 	fmt.Printf("Created keypair w/ pub address " + account.Address.Hex() + "\n")
@@ -66,25 +65,25 @@ func newAccount(_pass string) string {
 }
 
 // Close the ethereum wallet when we are finished
-func closeAccount(){
+func CloseAccount(ks *keystore.KeyStore, _pub string) bool {
 	// This returns an array with the keys stored in the keystore path
-	var ethAccArray = ks.Accounts()
 	var acc string = os.Getenv("ETHACC")
 
 	// Iterate through every account to find which pub key coincides
-	for i:= 0; i< len(ethAccArray); i++ {
+	for _, account := range ks.Accounts() {
 		// Search for the public key to lock
-		if acc == ethAccArray[i].Address.Hex() {
+		if _pub == account.Address.Hex() {
 			// If found, it is locked so that noone can access it.
-			err := ks.Lock(ks.Accounts()[i].Address)
+			err := ks.Lock(account.Address)
 			if err == nil {
-				os.Setenv("ETHACC", ethAccArray[i].Address.Hex())
+				os.Setenv("ETHACC", account.Address.Hex())
 				// Account is unlocked, we can get out of the iterating loop
 				fmt.Println("Account " + acc + " locked")
-				break
+				return true
 			} else {
 				fmt.Println("Account was found but not locked")
 			}
 		}
 	}
+	return false
 }
