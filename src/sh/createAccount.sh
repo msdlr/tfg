e6a1127fb9992306a8e21ddd7beb5ebbcd6f870e
msdlr <miguelsrosa97@gmail.com>
2021-02-08 16:58:44 +0100

    directory to store keystore

--- src/sh/createAccount.sh
+++ src/sh/createAccount.sh
@@ -3,7 +3,7 @@
 # Script to create an Ethereum wallet
 # $1 : location directory for the keypair
 
-[ -z "$1" ] && (echo "no datadir" ; exit)
+[ -z "$1" ] && geth -dev account new && exit
 
 geth -datadir "$1" -dev account new
 exit
