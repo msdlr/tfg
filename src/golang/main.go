9e140482f7d82ce8bc6943a1aba9e9499a57e4ba
msdlr <miguelsrosa97@gmail.com>
2021-02-21 13:23:59 +0100

    client setup and GeneralContract initialization

--- src/golang/main.go
+++ src/golang/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"log"
@@ -19,7 +21,9 @@ import (
 
 /* GLOBAL VARS */
 // keystore to use ethereum keys
-var ks *keystore.KeyStore
+var ks *keystore.KeyStore		//Keystore
+var myTrOps *bind.TransactOpts 	// Transaction Ops
+var myClient *ethclient.Client	// Client retrieved after dialing the RPC endpoint
 
 func main() {
 	/*
@@ -51,40 +55,74 @@ func main() {
 		os.Setenv("PRIVKEY","ad92041b60126af952f8320b473ccb555d7274a53f1c27e12d2f1ea8aaecda7b")
 	}
 
-	// Set up keystore with the correct path
+
+	/* Set up keystore with the correct path */
 	ks = keystore.NewKeyStore(os.Getenv("ETHKS"), keystore.StandardScryptN, keystore.StandardScryptP)
 
-	privateKey, _ := crypto.HexToECDSA( os.Getenv("PRIVKEY") )
-	chainId,_ :=strconv.Atoi(os.Getenv("CHAINID"))
+	// Retrieve TransactionOps and client object
+	myTrOps, myClient := setupClient()
 
-	transactOps, err := bind.NewKeyedTransactorWithChainID(privateKey,big.NewInt(int64(chainId)))
+	// DeployMain(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Main, error)
+	addr, trans, main, err := DeployMain(myTrOps, myClient)
+	main = main
+
+	if err == nil {
+		fmt.Println("## NEW CONTRACT DEPLOYED ##")
+		fmt.Println("Address: ", addr.Hex())
+		fmt.Println("Transaction hash: ", trans.Hash())
+		fmt.Println("Gas Used: ", trans.Gas(), "(price:", trans.GasPrice(), ")")
+		fmt.Println("Nonce: ", trans.Nonce())
+	}
+
+	myPubKey := getPubKeyFromPrivKey(os.Getenv("PRIVKEY"))
+
+	main.Initialize(myTrOps, *myPubKey, "msdlr")
+
+	myClient.Close()
+
+	/*
+
+		// Launch events checking
+		go checkEvents()
+
+		// Main routine stuck in inf loop
+		for true {
+			//fmt.Println("s")
+		}
+
+	*/
+}
+
+// setupClient retrieves the Transaction Ops and dials the RPC endpoint to establish the ethclient.client object
+func setupClient() (*bind.TransactOpts, *ethclient.Client) {
+	/* Set-up client */
+	privateKey, _ := crypto.HexToECDSA(os.Getenv("PRIVKEY"))
+	chainId, _ := strconv.Atoi(os.Getenv("CHAINID"))
+	transactOps, err := bind.NewKeyedTransactorWithChainID(privateKey, big.NewInt(int64(chainId)))
 	if err != nil {
 		log.Fatalf("Error getting TransactionOps: %v", err)
 	}
 
-	client, err := ethclient.Dial("HTTP://127.0.0.1:7545")
+	client, err := ethclient.Dial(os.Getenv("RPCENDPOINT"))
+	return transactOps, client
+}
+
+// getPubKeyFromPrivKey derives a public address corresponding to the private one passed as an argument
+func getPubKeyFromPrivKey(privKeyStr string)(pubk *common.Address ){
+	// String -> private key object
+	privateKey, err := crypto.HexToECDSA(privKeyStr)
 	if err != nil {
-		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+		log.Fatal(err)
 	}
-	client = client
-
-	// DeployMain(auth *bind.TransactOpts, backend bind.ContractBackend) (common.Address, *types.Transaction, *Main, error)
-	addr, trans,main,err :=DeployMain(transactOps,client)
-
-	fmt.Println(addr.Hex(),trans,main)
-
-
-	/*
-
-	// Launch events checking
-	go checkEvents()
-
-	// Main routine stuck in inf loop
-	for true {
-		//fmt.Println("s")
+	// Derive ECDSA public key
+	publicKeyDerived := privateKey.Public()
+	publicKeyECDSA, ok := publicKeyDerived.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("error casting public key to ECDSA")
 	}
-
-	*/
+	// Convert ECDSA public key to common.Address object (Ethereum)
+	pubAddr := crypto.PubkeyToAddress(*publicKeyECDSA)
+	return &pubAddr
 }
 
 // Start a goroutine to check for events in the Blockchain
