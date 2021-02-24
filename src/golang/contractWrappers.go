1da03d365d69494c96fb6e3ed9ebea40027a0ea0
msdlr <miguelsrosa97@gmail.com>
2021-02-24 21:56:26 +0100

    test cases for Add User

--- src/golang/contractWrappers.go
+++ src/golang/contractWrappers.go
@@ -7,9 +7,12 @@ import (
 	"github.com/ethereum/go-ethereum/ethclient"
 )
 
+var mainObj *Main // Main object used for invoking contract's methods
+
 // Deploys the an instance of generalContract and inicializes it with the public address of the creator
 func deployAndInitialize(myTrOps *bind.TransactOpts, myClient *ethclient.Client, ownerPubKeyStr string, ownerUserName string)(addr common.Address, deployTrans *types.Transaction, main *Main, deployError error, initTrans *types.Transaction, initError error){
 	addr, deployTrans, main, deployError = DeployMain(myTrOps, myClient)
+	mainObj = main // We need this main object to be preserved!!!
 	/*
 	if err == nil {
 		fmt.Println("## NEW CONTRACT DEPLOYED ##")
@@ -20,6 +23,13 @@ func deployAndInitialize(myTrOps *bind.TransactOpts, myClient *ethclient.Client,
 	}
 	 */
 	myPubKey := publicAddressFromString(ownerPubKeyStr)
-	initTrans,initError = main.Initialize(myTrOps, myPubKey, ownerUserName)
+	initTrans,initError = mainObj.Initialize(myTrOps, myPubKey, ownerUserName)
 	return
 }
+/*
+// AddUser(opts *bind.TransactOpts, _addr common.Address, _id string) (*types.Transaction, error)
+func contractAddUser (opts *bind.TransactOpts, _addr common.Address, _id string) (trans *types.Transaction,err error) {
+	trans, err = mainObj.AddUser(opts, _addr, _id)
+	return
+}
+*/
--- src/golang/contractWrappers_test.go
+++ src/golang/contractWrappers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"os"
 	"strings"
@@ -16,6 +17,7 @@ func initializeValidClient(endpoint string,chainId uint16,privkey string)(tops *
 	os.Setenv("RPCENDPOINT", endpoint)
 	chainIdStr := fmt.Sprintf("%d",chainId)
 	os.Setenv("CHAINID", chainIdStr)
+	os.Setenv("PRIVKEY", privkey)
 
 	// [1] in Ganache
 	tops, c = setupClient(privkey)
@@ -32,10 +34,10 @@ func TestDeployOk(t *testing.T){
 	// Act
 	// (addr common.Address, deployTrans *types.Transaction, main *Main, deployError error, initTrans *types.Transaction, initError error)
 	addr, deployTrans, main, deployError, initTrans, initError :=deployAndInitialize(to,c,"0xe065fAE3BaF52ee871C956E55C88548E4d17F5A5","TestOwner")
-	// Address is not 0x00...0
 
+	// Assert
 	addrStr := addr.String()
-	zeroStr := publicAddressFromString("0x0").Hex()
+	zeroStr := publicAddressFromString("0x0").Hex() // Address is not 0x00...0
 	zeroAddress := strings.Compare(addrStr,zeroStr) == 0
 	if  zeroAddress ||
 		deployTrans == nil ||
@@ -46,6 +48,97 @@ func TestDeployOk(t *testing.T){
 			t.Errorf("Failed to instantiate Contract")
 	}
 
-	// Assert
+	// Check owner
+	addrGet,err :=  main.GetContractAddress(nil)
+	addrGetStr := addrGet.String()
 
+	if addrGetStr != addrStr {
+		t.Errorf("Created contract address differs from getter")
+	}
+
+	if err != nil {
+		fmt.Println("Error:"+err.Error())
+	}
+}
+
+func TestAddUserOk(t *testing.T){
+	// Arrange: We need an initialized contract
+
+	ownerPub := "0xe065fAE3BaF52ee871C956E55C88548E4d17F5A5" // [1]
+	ownerPriv := "b7e6a03909b31f05c90354dd1a2bf61df5f223198c62551127250fdce2f6ffd4"
+
+	newUserPub := "0x12b3C6913a8eE35D1e0462f16Ac0aA6B6205a91a" // [2]
+	newUserName := "testUser"
+
+	to,c := initializeValidClient("http://localhost:7545",5777,ownerPriv)
+	_, _, main, _, _, _ :=deployAndInitialize(to,c,ownerPub,"TestOwner")
+
+	// Act: Call contract method AddUser
+	userAddr := publicAddressFromString(newUserPub)
+	_, addUserErr := main.AddUser(to, userAddr, newUserName)
+
+	// Assert
+	if addUserErr != nil {
+		t.Errorf("Could not add user."+ addUserErr.Error())
+	}
+
+	// Check if registered via getter
+	isUserRegistered, userRegisteredErr := main.GetUserRegistered(nil, userAddr)
+
+	if userRegisteredErr != nil {
+		t.Errorf("Error retrieving if user is registered "+ userRegisteredErr.Error())
+	}
+
+	if !isUserRegistered {
+		t.Errorf("User is not registered (false, expected true)")
+	}
+
+	// Check if admin via getter
+	isUserAdmin, userAdminErr := main.GetUserAdmin(nil, userAddr)
+
+	if userAdminErr != nil {
+		t.Errorf("Error retrieving if user is registered "+ userAdminErr.Error())
+	}
+
+	if isUserAdmin {
+		t.Errorf("User is admin (true, expected false)")
+	}
+
+	// Check user attempts via getter
+	attempts, attemptsErr := main.GetUserAttempts(nil, userAddr)
+
+	if attemptsErr != nil {
+		t.Errorf("Error getting user attempts"+ userAdminErr.Error())
+	}
+
+	if attempts > 0 {
+		t.Errorf("Error, attempts="+string(attempts) +", expected 0"+ userAdminErr.Error())
+	}
+
+	// Check getters for relating pubkey and username
+	getUserIdStr, getUserIdErr :=main.GetUserId(nil, userAddr)
+	getUserAddress, getUserAddressErr := main.GetUserAddress(nil,newUserName)
+
+	if getUserIdErr != nil {
+		t.Errorf("Error relating user public address to username "+ getUserIdErr.Error())
+	}
+
+	if getUserAddressErr != nil {
+		t.Errorf("Error relating username to public address"+ getUserAddressErr.Error())
+	}
+
+	getUserStr:= getUserAddress.String()
+
+	if getUserIdStr != newUserName || getUserStr != newUserPub {
+		t.Errorf("User ID and public key do not match")
+	}
+
+	// Check AuthContract
+
+	userAuthContract,_ := main.GetUserAuthContract(nil, userAddr)
+
+	userAuthStr := userAuthContract.String()
+	if userAuthStr == common.BytesToAddress([]byte("0")).String() {
+		t.Errorf("Error retrieving user's AuthContract")
+	}
 }
